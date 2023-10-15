b45e2f38b72bb98866f3e6bb69323a9340961155
willyjo1 <arthurwilchield@gmail.com>
2023-10-15 19:56:19 +0000

    completed the onnboarding screen

--- lib/view/loading_page.dart
+++ lib/view/loading_page.dart
@@ -1,4 +1,6 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:ui_challenge/global_variables/colors.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -8,23 +10,56 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-
   @override
   void initState() {
     super.initState();
     load();
   }
 
-  void load() async{
-    await Future.delayed( const Duration(seconds: 3), (){
+  void load() async {
+    await Future.delayed(const Duration(seconds: 4), () {
       Navigator.pushNamed(context, '/onboardingScreen');
     });
   }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(color: Colors.green),
-      child: const Center(child: Text("Loading Screen", style: TextStyle(decoration: TextDecoration.none, color: Colors.white),)),
-    );
+    return Stack(children: [
+      Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: const AssetImage("assets/plant_tree.avif"),
+                fit: BoxFit.cover,
+                filterQuality: FilterQuality.high,
+                colorFilter: ColorFilter.mode(
+                    secondaryWidgetColor.withOpacity(0.4), BlendMode.darken))),
+      ),
+      Padding(
+        padding: const EdgeInsets.symmetric(vertical:150.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            AnimatedTextKit(
+              totalRepeatCount: 1,
+              isRepeatingAnimation: false,
+              animatedTexts: [
+                RotateAnimatedText("Enjoy the breeze of the plants",
+                    textAlign: TextAlign.center,
+                    textStyle: const TextStyle(
+                      decoration: TextDecoration.none,
+                      color: Colors.white,
+                      fontSize: 45
+                    ),
+                    transitionHeight: 150,
+                    duration: const Duration(seconds: 2),
+                    rotateOut: false)
+              ],
+            )
+          ],
+        ),
+      ),
+  
+    ]);
   }
 }
--- lib/view/onboarding_screen.dart
+++ lib/view/onboarding_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:ui_challenge/global_variables/colors.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
@@ -8,8 +10,115 @@ class OnboardingScreen extends StatefulWidget {
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
+  final PageController pageController = PageController();
   @override
   Widget build(BuildContext context) {
-    return  const Scaffold();
+    return Scaffold(
+      backgroundColor: primaryBackgroundColor,
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(50, 70, 40, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerRight,
+              child: GestureDetector(
+                onTap: () => pageController.animateToPage(2,
+                    duration: const Duration(milliseconds: 500),
+                    curve: Curves.easeIn),
+                child: Text(
+                  "Skip",
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey.shade700,
+                      fontWeight: FontWeight.w600),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 480,
+              child: PageView(
+                controller: pageController,
+                onPageChanged: (value) {},
+                children: [
+                  ColorFiltered(
+                      colorFilter: const ColorFilter.mode(
+                          primaryBackgroundColor, BlendMode.darken),
+                      child: Image.asset("assets/faux_watermelon.avif")),
+                  ColorFiltered(
+                      colorFilter: const ColorFilter.mode(
+                          primaryBackgroundColor, BlendMode.darken),
+                      child: Image.asset("assets/pepper_face_plant.avif")),
+                  ColorFiltered(
+                      colorFilter: const ColorFilter.mode(
+                          primaryBackgroundColor, BlendMode.darken),
+                      child: Image.asset("assets/faux_watermelon.avif"))
+                ],
+              ),
+            ),
+            Center(
+              child: SmoothPageIndicator(
+                controller: pageController,
+                onDotClicked: (index) => pageController.animateToPage(index,
+                    duration: const Duration(milliseconds: 500),
+                    curve: Curves.easeIn),
+                effect: const ExpandingDotsEffect(
+                    spacing: 10,
+                    dotHeight: 11,
+                    dotColor: secondaryBackgroundColor,
+                    dotWidth: 9,
+                    activeDotColor: mainWidgetColor),
+                count: 3,
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 30.0),
+                child: RichText(
+                    text: const TextSpan(
+                        style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 40,
+                            color: complementaryWidgetColor),
+                        children: [
+                      TextSpan(text: "Enjoy your\nLife with "),
+                      TextSpan(
+                          text: "Plants",
+                          style: TextStyle(fontWeight: FontWeight.bold))
+                    ])),
+              ),
+            ),
+            const Spacer(
+              flex: 4,
+            ),
+            Container(
+              height: 70,
+              width: 70,
+              decoration: BoxDecoration(
+                  color: mainWidgetColor,
+                  borderRadius: BorderRadius.circular(100)),
+              child: IconButton(
+                  onPressed: () async{
+                    pageController.nextPage(
+                        duration: const Duration(milliseconds: 500),
+                        curve: Curves.easeIn);
+                    await Navigator.pushNamed(context, "/homescreen");
+                  },
+                  icon: const Icon(
+                    Icons.arrow_forward,
+                    size: 35,
+                    color: Colors.white,
+                  )),
+            ),
+            const Spacer(
+              flex: 4,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

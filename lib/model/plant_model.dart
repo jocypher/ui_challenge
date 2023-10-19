47d8cb72f5d46b41b8f72fef7fc486072a906018
willyjo1 <arthurwilchield@gmail.com>
2023-10-19 15:51:11 +0000

    navigate to details screen

--- lib/model/plant_model.dart
+++ lib/model/plant_model.dart
@@ -1,8 +1,14 @@
+import 'package:flutter/material.dart';
+
 class Plant{
   final String name;
   final String plantImage;
   final double price;
 
   Plant({required this.name, required this.plantImage, required this.price});
+
+  void navigateTODetailsScreen(BuildContext context){
+    Navigator.pushNamed(context, "/detailsScreen", arguments: this);
+  }
   
 }
